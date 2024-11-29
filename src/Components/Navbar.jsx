6543d3d8d8eeeba49af52ecb64976b572d2b55aa
laura magallanes <pasocenturiontours@gmail.com>
2024-11-29 09:11:01 -0300

    Quedan implementados los Link en el navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { routes } from '../utils/routes'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -6,7 +8,10 @@ const Navbar = () => {
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to={routes.inicio}> <h4>Home</h4> </Link>
+      <Link to={routes.contacto}> <h4>Contacto</h4> </Link>      
+      <Link to={routes.destacados}> <h4>Favs</h4> </Link>
+
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>

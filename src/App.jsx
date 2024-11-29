78e513ec682db30a3160335ca3fdf4af35f731fe
laura magallanes <pasocenturiontours@gmail.com>
2024-11-29 09:05:06 -0300

    rutas creadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
+import { Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
+import { routes } from "./utils/routes";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+        
+          <Routes>
+            <Route path={routes.inicio} element={<Home/>}/>
+            <Route path={routes.contacto} element={<Contact />}/>
+            <Route path={routes.destacados} element={<Favs />}/>
+            <Route path={routes.detalleDentista} element={<Detail />}/>
+            <Route
+          path={routes.pnf}
+          element={<h1>Page not found - Error 404</h1>}
+        />
+          </Routes>
           <Footer/>
       </div>
   );
---
+++ src/utils/routes.js
@@ -0,0 +1,7 @@
+export const routes = {
+    inicio: "/",
+    contacto: "/contact",
+    destacados: "/favs",
+    detalleDentista: "/dentist/:id", //ruta dinámica para usar con useParams
+    pnf: "*", // error 404
+  };

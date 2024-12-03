087f876cddac280810ee91c50f58745112e7ff83
nikollquinteroc <nikollquinteroc@gmail.com>
2024-12-02 20:48:17 -0500

    Añadiendo formulario

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,51 @@
-
+import React, { useState } from 'react'
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [usuario, setUsuario] = useState({
+    nombreCompleto: "",
+    email: "",
+  });
 
+  const [mostrarMensaje, setMostrarMensaje] = useState(false);
+  const [error, setError] = useState(false);
+  console.log(usuario);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (usuario.nombreCompleto.trim().length > 5 &&
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(usuario.email)) {
+      setMostrarMensaje(true);
+      setError(false);
+    } else {
+      setMostrarMensaje(false);
+      setError(true);
+    }
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <label>Nombre Completo:</label>
+        <input
+          type="text"
+          value={usuario.nombreCompleto}
+          onChange={(event) =>
+            setUsuario({ ...usuario, nombreCompleto: event.target.value })
+          } />
+        <label>Email:</label>
+        <input
+          type="text"
+          value={usuario.email}
+          onChange={(event) =>
+            setUsuario({ ...usuario, email: event.target.value })
+          }
+        />
+        <button>Enviar</button>
+        {error ? (
+          <h4 style={{ color: "red" }}> Por favor verifique su información nuevamente</h4>
+        ) : null
+        }
       </form>
+      {mostrarMensaje && <h4> Gracias {usuario.nombreCompleto}, te contactaremos cuando antes vía mail</h4>}
     </div>
   );
 };
